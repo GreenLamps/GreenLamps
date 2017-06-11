bb700c7acf6c2ef7fe0a885eeb46f225353caf41
YTZhYJ <zhangyj_beijing@126.com>
2017-06-11 23:20:26 +0800

    调整 “研究中心”

--- src/app/lcl-research/lcl-research.route.ts
+++ src/app/lcl-research/lcl-research.route.ts
@@ -3,7 +3,11 @@
  */
 import {Route} from '@angular/router';
 import {LclResearchComponent} from './lcl-research.component';
+import {ContentGreenLightsResolvePagingParams} from '../config/content-green-lamps-resolve-paging-params';
 export const LCLRESEARCH_ROUTE: Route = {
     path: 'lcl-research',
     component: LclResearchComponent,
+    resolve: {
+      'pagingParams': ContentGreenLightsResolvePagingParams,
+    },
   };
